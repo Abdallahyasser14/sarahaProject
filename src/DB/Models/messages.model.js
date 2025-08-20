36b6d29cae5f3f934bf3361c7a425aa3d65515c5
abdallah yasser <abdallahshady4@gmail.com>
2025-08-20 07:46:43 +0300

    build message schema

--- src/DB/Models/messages.model.js
+++ src/DB/Models/messages.model.js
@@ -1,2 +1,29 @@
-// JavaScript template
-console.log('Hello, World!');
+import mongoose from 'mongoose';
+
+// create the schema for the message
+const messageSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+ 
+    receiverId: { //? this is the id of the user who received the message fd2a relationship `reference document`
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        ref:"User" //? this is the name of the collection in the database that the model is based on
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// create the model from the schema
+const Message = mongoose.model('Message', messageSchema);
+
+export default Message;
